f6630fe06a22afcc2a8e9ba5e7001e4fe2cc3877
FMirai <djmf.mmf@gmail.com>
2017-09-29 14:12:08 +0900

    Basic Version
    The very basic was completed

---
+++ DoublyLinkedList.java
@@ -0,0 +1,29 @@
+public class DoublyLinkedList{
+
+  Node first;
+  Node tail;
+
+  public void add(String newFloorNum, int newEvent){
+    /* ノードの生成とノードの初期化 */
+    Node newNode = new Node();
+    newNode.event = newEvent;
+    newNode.floorNum = newFloorNum;
+
+    /* 追加する前のリストの要素数が0個の時と1以上の時で場合分け */
+    if(first == null){
+      first = newNode;
+      tail = newNode;
+    }else{
+      tail.next = newNode;
+      newNode.prev = tail;
+      tail = newNode;
+    }
+  }
+}
+
+class Node{
+  Node next;
+  Node prev;
+  int event;
+  String floorNum;
+}
